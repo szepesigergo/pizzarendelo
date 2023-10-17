aaf88111e1f9d96b4da9faa2a7fac8686ef6d80c
Szepesi Gergő Manó  (SZF_2023_2025) <szepesi.gergo.mano@diak.szamalk-szalezi.hu>
2023-10-17 13:36:25 +0200

    pizzavegso

--- src/pizza/pizza.java
+++ src/pizza/pizza.java
@@ -96,6 +96,11 @@ public class pizza extends javax.swing.JFrame {
         });
 
         jButton4.setText("magyaros");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("Kukorica-sonka sajt,");
 
@@ -110,6 +115,11 @@ public class pizza extends javax.swing.JFrame {
         jLabel9.setText("sajt, hagyma");
 
         jRadioButton6.setText("bacon");
+        jRadioButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton6ActionPerformed(evt);
+            }
+        });
 
         jRadioButton8.setText("jalapeno");
         jRadioButton8.addActionListener(new java.awt.event.ActionListener() {
@@ -128,14 +138,30 @@ public class pizza extends javax.swing.JFrame {
 
         buttonGroup1.add(jRadioButton2);
         jRadioButton2.setText("+sajt");
+        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton2ActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(jRadioButton3);
         jRadioButton3.setText("csipős extra");
+        jRadioButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton3ActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(jRadioButton4);
         jRadioButton4.setText("hagyma nélkül");
+        jRadioButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton4ActionPerformed(evt);
+            }
+        });
 
         btnRendeles.setText("Rendelés leadása");
+        btnRendeles.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         btnRendeles.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnRendelesActionPerformed(evt);
@@ -295,11 +321,31 @@ public class pizza extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
-        // TODO add your handling code here:
+       String uzenet;
+        if(true){
+            uzenet = "pizza összeg: 200Ft";
+        }
+        else{
+            uzenet = "Sikertelen rendelés";
+        }
+        
+        String cim = "FIGYELEM!";
+        int ikonTipus = 1;
+        JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
     }//GEN-LAST:event_jRadioButton1ActionPerformed
 
     private void jRadioButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton8ActionPerformed
-        // TODO add your handling code here:
+        String uzenet;
+        if(true){
+            uzenet = "pizza összeg: 200Ft";
+        }
+        else{
+            uzenet = "Sikertelen rendelés";
+        }
+        
+        String cim = "FIGYELEM!";
+        int ikonTipus = 1;
+        JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
     }//GEN-LAST:event_jRadioButton8ActionPerformed
 
     private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
@@ -318,10 +364,21 @@ public class pizza extends javax.swing.JFrame {
         String cim = "FIGYELEM!";
         int ikonTipus = 1;
         JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
+        System.exit(0);
     }//GEN-LAST:event_btnRendelesActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        JOptionPane.showMessageDialog(rootPane, "teszt","FIGYELMEZTETÉS", 2);
+       String uzenet;
+        if(true){
+            uzenet = "pizza összeg: 1500Ft";
+        }
+        else{
+            uzenet = "Sikertelen rendelés";
+        }
+        
+        String cim = "FIGYELEM!";
+        int ikonTipus = 1;
+        JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
@@ -337,6 +394,76 @@ public class pizza extends javax.swing.JFrame {
         int ikonTipus = 1;
         JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
     }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        String uzenet;
+        if(true){
+            uzenet = "pizza összeg: 1500Ft";
+        }
+        else{
+            uzenet = "Sikertelen rendelés";
+        }
+        
+        String cim = "FIGYELEM!";
+        int ikonTipus = 1;
+        JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
+       String uzenet;
+        if(true){
+            uzenet = "pizza összeg: 200Ft";
+        }
+        else{
+            uzenet = "Sikertelen rendelés";
+        }
+        
+        String cim = "FIGYELEM!";
+        int ikonTipus = 1;
+        JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
+    }//GEN-LAST:event_jRadioButton2ActionPerformed
+
+    private void jRadioButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton3ActionPerformed
+        String uzenet;
+        if(true){
+            uzenet = "Figyelem hagyma nélkül kérte!";
+        }
+        else{
+            uzenet = "Sikertelen rendelés";
+        }
+        
+        String cim = "FIGYELEM!";
+        int ikonTipus = 1;
+        JOptionPane.showMessageDialog(null, uzenet, cim, 2);
+    }//GEN-LAST:event_jRadioButton3ActionPerformed
+
+    private void jRadioButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton4ActionPerformed
+        String uzenet;
+        if(true){
+            uzenet = "Figyelem hagyma nélkül rendeled meg";
+        }
+        else{
+            uzenet = "Sikertelen rendelés";
+        }
+        
+        String cim = "FIGYELEM!";
+        int ikonTipus = 1;
+        JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
+    }//GEN-LAST:event_jRadioButton4ActionPerformed
+
+    private void jRadioButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton6ActionPerformed
+        String uzenet;
+        if(true){
+            uzenet = "pizza összeg: 200Ft";
+        }
+        else{
+            uzenet = "Sikertelen rendelés";
+        }
+        
+        String cim = "FIGYELEM!";
+        int ikonTipus = 1;
+        JOptionPane.showMessageDialog(null, uzenet, cim, ikonTipus);
+    }//GEN-LAST:event_jRadioButton6ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -371,6 +498,14 @@ public class pizza extends javax.swing.JFrame {
                 new pizza().setVisible(true);
             }
         });
+    }
+
+    public static int getALLBITS() {
+        return ALLBITS;
+    }
+
+    public static int getABORT() {
+        return ABORT;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
